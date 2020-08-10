017bc92b590a0a93ec1244f2007a39a8b7567a67
Deniz Akşimşek <d.aksimsek1@outlook.com>
2020-08-10 21:20:47 +0300

    Try to fix datetimes

--- app/endpoints/micropub/process-data/datetime.js
+++ app/endpoints/micropub/process-data/datetime.js
@@ -1,9 +1,6 @@
 const moment = require('moment');
 
-/*
-Convert and format time to local Kiwi format
-*/
 exports.formatDateTime = function formatDateTime() {
-    const dateTime = moment(new Date()).tz('Pacific/Auckland').format('YYYY-MM-DDTHH:mm:ss');
+    const dateTime = moment.utc().utcOffset('+0300').format('YYYY-MM-DDTHH:mm:ss');
     return dateTime;
 };
