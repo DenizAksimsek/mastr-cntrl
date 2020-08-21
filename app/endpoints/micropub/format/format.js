706056e2d712725886f7d5b35eac5fe7d5288ac5
Deniz Akşimşek <d.aksimsek1@outlook.com>
2020-08-21 14:58:16 +0300

    hope this works

--- app/endpoints/micropub/format/format.js
+++ app/endpoints/micropub/format/format.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 const logger = require(appRootDirectory + '/app/logging/bunyan');
 const functionPath = '/app/endpoints/micropub/process-data/';
 const handleContent = require(appRootDirectory + functionPath + 'content');
@@ -7,15 +9,25 @@ const handleTargets = require(appRootDirectory + functionPath + 'syndication-tar
 
 // Entry to be moved in to a formatter function, and return the markdown. It needs to take inputs from all the functions outputs previously.
 
+function ensureArray(x) {
+	return x instanceof Array ? x : [x]
+}
+
 module.exports = function format(micropubContent) {
     logger.info('JSON received: ' + JSON.stringify(micropubContent));
+    const frontmatter = `
+date: ${moment(mp.date).toISOString()}
+${mp.name && `name: ${mp.name}`}
+${mp.category && `tags: [${ensureArray(mp.name).join(', ')}]`}
+${mp['in-reply-to'] && `replyTo: ${mp['in-reply-to']}`}
+${mp['like-of'] && `likeOf: ${mp['like-of']}`}
+${mp.syndication && `syndication: [${ensureArray(mp.syndication).join(', ')}]`}
+`.replace(/\n+/, '\n')
 
-    const date = handleDateTime.formatDateTime();
-    const content = handleContent.formatContent(micropubContent);
-    const tags = handleTags.formatTags(micropubContent);
-    const targetArray = handleTargets.formatTargets(micropubContent);
+    const entry = `---${frontmatter}---
 
-    const entry = JSON.stringify({ date, tags, content })
+${mp.content}
+`
 
     logger.info('Note formatter finished: ' + entry);
     return entry;
